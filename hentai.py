07070253a6c0344c7c05522344d7d1829ee582dd
hentaichan <70948244+hentai-chan@users.noreply.github.com>
2020-09-11 20:19:42 +0200

    Rewrites the Hentai class from scratch by using the API instead of parsing HTML.

--- hentai.py
+++ hentai.py
@@ -1,38 +1,100 @@
-from urllib.parse import urlparse
+from datetime import datetime
+from enum import Enum, unique
+from types import resolve_bases
+from typing import List
+from urllib.parse import urljoin, urlparse
 
-from requests_html import HTMLSession
+import requests
 
 
-class Hentai:
-    def __init__(self, id):
+@unique
+class Format(Enum):
+    English = 'english'
+    Japanese = 'japanese'
+    Pretty = 'pretty'
+
+@unique
+class Extension(Enum):
+    JPG = 'j'
+    PNG = 'p'
+    GIF = 'g'
+
+class Hentai(object):
+    _url = lambda id: urljoin("https://nhentai.net/api/gallery/", str(id))
+
+    def __init__(self, id: int):
         self.id = id
-        self.url = f"https://nhentai.net/g/{self.id}/"
+        self.url = urljoin("https://nhentai.net/g/", str(self.id))
+        self.api = Hentai._call_api(Hentai._url(self.id))      
         
-    @property
-    def _session(self):
-        return HTMLSession().get(self.url)
+    @staticmethod
+    def _call_api(url) -> dict:
+        _response = requests.get(url)
+        _response.encoding = 'utf-8'
+        if _response.status_code != 200:
+            raise requests.ConnectionError(f"Expected status code 200, but got {_response.status_code}")
+        else:
+            return _response.json()
 
     @property
-    def html(self):
-        return self._session.html
+    def media_id(self) -> int:
+        return self.api['media_id']
+
+    def title(self, format: Format) -> str:
+        return self.api['title'].get(format.value)
 
     @property
-    def favorites(self):
-        return self.html.find('.nobold', first = False)[1].text.strip('(').strip(')')
+    def cover(self) -> str:
+        cover_ext = Extension(self.api['images']['cover']['t']).name.lower()
+        return f"https://t.nhentai.net/galleries/{self.media_id}/cover.{cover_ext}"
 
     @property
-    def title(self):
-        return self.html.find('h1.title', first = True).text
+    def thumbnail(self) -> str:
+        thumb_ext = Extension(self.api['images']['thumbnail']['t']).name.lower()
+        return f"https://t.nhentai.net/galleries/{self.media_id}/thumb.{thumb_ext}"
 
     @property
-    def pretty(self):
-        return self.html.find('span.pretty', first = True).text
+    def upload_date(self) -> datetime:
+        raise NotImplementedError()
+
+    _name = lambda self, type: [tag['name'] for tag in self.api['tags'] if tag['type'] == type]
 
     @property
-    def cover(self):
-        return self.html.find('.lazyload', first = True).attrs['data-src']
+    def tags(self) -> List[str]:
+        return Hentai._name(self, 'tag')
+
+    @property
+    def language(self) -> List[str]:
+        return Hentai._name(self, 'language')
+
+    @property
+    def artist(self) -> str:
+        return Hentai._name(self, 'artist')[0]
+
+    @property
+    def category(self) -> List[str]:
+        return Hentai._name(self, 'category')
+
+    @property
+    def num_pages(self) -> int:
+        return self.api['num_pages']
+
+    @property
+    def num_favorites(self) -> int:
+        return self.api['num_favorites']
+
+    _extension = lambda self: Extension(self.api['images']['pages'][0]['t']).name.lower()
+
+    @property
+    def image_urls(self) -> List[str]:
+        page_url = lambda num: f"https://i.nhentai.net/galleries/{self.media_id}/{num}.{Hentai._extension(self)}"
+        return [page_url(num) for num in range(self.num_pages + 1)] 
 
     @staticmethod
-    def get_random_id():
-        random_url = HTMLSession().get("https://nhentai.net/random").url
-        return int(urlparse(random_url).path[3:-1])
+    def random_id() -> int:
+        _session = requests.get("https://nhentai.net/random/")
+        return int(urlparse(_session.url).path[3:-1])
+
+
+if __name__ == '__main__':
+    pass
